406a220bf11f943b3faef1fa338815adbe008739
Polux <contato@viniciuspolux.com>
2018-07-04 23:09:02 -0300

    Update simpson.py

--- simpson.py
+++ simpson.py
@@ -1,43 +1,39 @@
-# Método de Integração de Simpson
-# Função de integração: x * f(x) 
-# Mudar função dentro do código
+x = []
+fx = []
+i = 0
+I = 0
+P = 0
 
-while True:
+#inputs dos pontos do intervalo e o número de partes em que é dividido
+a = float(input('Valor de A: '))
+b = float(input('Valor de B: '))
+n = int(input('Partes divididas: '))
 
-    x = []
-    fx = []
-    i = 0
-    I = 0
-    P = 0
-    n = int(input('Número de pontos: '))
+#formula ponto medio
+h = (b - a) / (n)
 
-    if (n % 2) != 0:
+while i < n:
+    #input para os pontos da função
+    num_x = float(input('Digite x{0}: '.format(i)))
+    num_fx = float(input('Digite fx{0}'.format(i)))
+    x.append(num_x)
+    fx.append(num_fx)
 
-        a = float(input('Valor inicial: ')) 
-        b = float(input('Valor final: '))
-        h = (b - a)/n
+    #se o valor de i for diferente de zero, seja par e diferente do numero de partes
+    #em que o intervalo é dividido a função do pol é utilizada
+    if (i != 0) and ((i % 2) == 0) and (i != (n)):
+        P += x[i] * fx[i]
 
-        while i < n:
-            num_x = float(input(' x{0}: '.format(i)))
-            num_fx = float(input(' fx{0}: '.format(i)))
-            x.append(num_x)
-            fx.append(num_fx)
+    # //               , seja impar e diferente do numero de partes
+    #em que o intervalo é dividido, a função de integração é utilizada
+    elif (i != 0) and ((i % 2) != 0) and (i != (n)):
+        I += x[i] * fx[i]
 
-            if (i != 0) and ((i % 2) == 0) and (i != (n - 1)):
-                P += x[i] * fx[i]
-            elif (i != 0) and ((i % 2) != 0) and (i != (n - 1)):
-                I += x[i] * fx[i]
+    i += 1
 
-            i += 1
+E = (x[0] * fx[0]) + (x[n-1] * fx[n-1])
 
-        E = (x[0] * fx[0]) + (x[n-1] * fx[n-1])
+#formula simpson
+Simpson = (h / 3) * (E + (4 * I) + (2 * P))
 
-        Simpson = (h / 3) * (E + (4 * I) + (2 * P))  
-
-        print('O valor da integral é: {0:.3f}'.format(Simpson))
-        
-        break
-        
-    else:
-        
-        print('O número de pontos precisa ser ímpar.')  
+print('O valor da integral é: {0:.3f}'.format(Simpson))
